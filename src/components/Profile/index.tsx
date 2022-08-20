1b51d41d5d5e29dbde909706443a5b6f3e55e991
Gian Lucas <lgian584@gmail.com>
2022-08-19 23:20:12 -0300

    get user data from github api

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,36 +2,62 @@ import { ProfileContainer } from "./styles";
 import { HiExternalLink, HiOfficeBuilding } from "react-icons/hi";
 import { AiFillGithub } from "react-icons/ai";
 import { FaUserFriends } from "react-icons/fa";
+import { api } from "../../lib/axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  name: string;
+  login: string;
+  followers: number;
+  bio: string;
+  avatar_url: string;
+  company: string | null;
+}
 
 export function Profile() {
+  const [user, setUser] = useState({} as User);
+
+  async function getGithubUser() {
+    const res = await api.get("/users/gian-lucas");
+    const data: User = res.data;
+
+    setUser(data);
+  }
+
+  useEffect(() => {
+    getGithubUser();
+  }, []);
+
+  if (!user.name) {
+    return <h1>Carregando dados do usuário...</h1>;
+  }
+
   return (
     <ProfileContainer>
-      <img src="https://avatars.githubusercontent.com/u/67169105?v=4" alt="" />
+      <img src={user.avatar_url} alt={user.name} />
       <div>
         <header>
-          <strong>Gian Lucas</strong>
-          <a href="https://github.com/gian-lucas" target="_blank">
+          <strong>{user.name}</strong>
+          <a href={`https://github.com/${user.login}`} target="_blank">
             GITHUB <HiExternalLink size={14} />
           </a>
         </header>
 
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{user.bio}</p>
 
         <footer>
           <span>
-            <AiFillGithub size={18} /> gian-lucas
+            <AiFillGithub size={18} /> {user.login}
           </span>
-          {/* <span>
-            <HiOfficeBuilding size={18} />
-            Rockeseat
-          </span> */}
+          {user.company && (
+            <span>
+              <HiOfficeBuilding size={18} />
+              {user.company}
+            </span>
+          )}
           <span>
             <FaUserFriends size={18} />
-            32 seguidores
+            {user.followers} seguidores
           </span>
         </footer>
       </div>
